66a8ff706a5f44ca7b37832d26159d8ef4b97b39
arunsamdoss <arun.samdoss@neethtechnologies.com>
2021-01-15 11:22:19 +0530

    first commit1

---
+++ src/views/PageOne.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="pageone"><h1>hello this pageone</h1></div>
+
+</template>
+<script>
+export default {
+    
+}
+</script>
---
+++ src/views/PageTwo.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="pagetwo"><h1>hello this pagetwo</h1></div>
+
+</template>
+<script>
+export default {
+    
+}
+</script>
